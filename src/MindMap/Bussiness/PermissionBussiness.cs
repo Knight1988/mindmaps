0a93cf8f05ee594dc3c705cc9c303a8d7227a93b
Squall Leonhart <Knight1988@gmail.com>
2016-09-20 11:54:18 +0700

    create check vip method

---
+++ src/MindMap/Bussiness/PermissionBussiness.cs
@@ -0,0 +1,11 @@
+﻿namespace MindMap.Bussiness
+{
+    public static class PermissionBussiness
+    {
+        public static bool IsVip(int userId)
+        {
+            // TODO: check if user is vip
+            return userId == 1;
+        }
+    }
+}
